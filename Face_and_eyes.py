8595f816c5545d2eab1ca18d88523d76f7445ecf
yash-007 <yashkelkar76@gmail.com>
2019-10-19 23:59:56 +0530

    first commit

---
+++ Face_and_eyes.py
@@ -0,0 +1,11 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    cv2.imshow("frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
